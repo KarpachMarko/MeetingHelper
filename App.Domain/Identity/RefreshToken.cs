e6df7cc76eef845903fd9835ca7222ce2e386491
Marko Karpats <marko_karpats@outlook.com>
2022-08-05 20:30:05 +0300

    Meeting entity

---
+++ App.Domain/Identity/RefreshToken.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Base.Contracts.Domain;
+using Base.Domain;
+
+namespace App.Domain.Identity;
+
+public class RefreshToken : DomainEntityId, IDomainEntityUser<AppUser>
+{
+    [StringLength(36, MinimumLength = 36)] 
+    public string Token { get; set; } = Guid.NewGuid().ToString();
+    public DateTime TokenExpirationDateTime { get; set; } = DateTime.Now.AddDays(7);
+    
+    [StringLength(36, MinimumLength = 36)] 
+    public string? PreviousToken { get; set; }
+    public DateTime? PreviousTokenExpirationDateTime { get; set; }
+    
+    public Guid UserId { get; set; }
+    public AppUser? User { get; set; }
+}
---
+++ App.Domain/Meeting.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Base.Domain;
+
+namespace App.Domain;
+
+public class Meeting : DomainEntityId
+{
+    [MaxLength(256)]
+    [Display(ResourceType = typeof(Base.Resources.EntityCommon.EntityCommon), Name = "Title")]
+    public string Title { get; set; } = default!;
+    
+    [MaxLength(2512)]
+    [Display(ResourceType = typeof(Base.Resources.EntityCommon.EntityCommon), Name = "Title")]
+    public string Description { get; set; } = default!;
+
+    public DateTime StartDate { get; set; }
+    
+    public DateTime EndDate { get; set; }
+    
+    public double BudgetPerPerson { get; set; }
+}
